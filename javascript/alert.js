c294f387e18aa896351d61d2d85b45d88dcebdf5
keily6am <143549225+keily6am@users.noreply.github.com>
2023-11-15 21:05:59 +0000

    modified alert

--- javascript/alert.js
+++ javascript/alert.js
@@ -5,5 +5,14 @@ for(let i = 1; i<= 100; i++)
     sum = sum + i ; 
 }
 
-alert("Answer = " + sum ); 
-console.log(" Answer = " + sum);
+//alert("Answer = " + sum ); 
+//console.log(" Answer = " + sum);
+
+//answer will refer to the p tag in the html document (down below)
+let answer = document.querySelector('p');
+//change the interHTML of the <p>
+answer.innerHTML = " Answer = " + sum;
+
+// change the css of the <p>
+answer.style.color ="red";
+answer.style.fontSize = "40px";
