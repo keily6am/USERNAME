b12eb4cc374c8999a62b93525448c7680e8628f9
keily6am <143549225+keily6am@users.noreply.github.com>
2023-11-15 20:50:38 +0000

    modified alert

--- javascript/alert.js
+++ javascript/alert.js
@@ -2,8 +2,8 @@ let sum = 0;
 
 for(let i = 1; i<= 100; i++)
 {
-    sum = sum + i; 
+    sum = sum + i ; 
 }
 
-//alert("Answer = " + sum ); 
+alert("Answer = " + sum ); 
 console.log(" Answer = " + sum);
