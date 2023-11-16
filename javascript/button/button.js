ca2cdc99d4e72a7797986d7717b3d2ede17b2fcf
keily6am <143549225+keily6am@users.noreply.github.com>
2023-11-16 22:58:01 +0000

    modified button modified button

--- javascript/button/button.js
+++ javascript/button/button.js
@@ -1,13 +1,13 @@
 let h1_text = document.querySelector("#text");
 let p_tag = document.querySelector("p");
 let btn = document.querySelector(" #myButton"); 
-let btn2 = document.querySelector(" #greenbut");
-let btn3 = document.querySelector(" #bluebut");  
+let button = document.querySelector(" #greenbut"); 
+let buut = document.querySelector(" #bluebut"); 
 
 //p_tag.innerHTML = h1_text; 
-btn.addEventListener("click",button_clicked);
+btn.addEventListener("click",btn_clicked);
 
-function button_clicked()
+function btn_clicked()
 {
     if(h1_text.textContent == "Hello")
     {
@@ -15,5 +15,30 @@ function button_clicked()
         h1_text.style.color = "red";
     } else{
         h1_text.textContent = "Hello";
+        h1_text.style.color = "white";
+    }
+}
+button.addEventListener("click",button_clicked);
+function button_clicked()
+{
+    if(h1_text.textContent == "Hello")
+    {
+        h1_text.textContent = "Bye";
+        h1_text.style.color = "green";
+    } else{
+        h1_text.textContent = "Hello";
+        h1_text.style.color = "white";
+    }
+}
+buut.addEventListener("click",buut_clicked);
+function buut_clicked()
+{
+    if(h1_text.textContent == "Hello")
+    {
+        h1_text.textContent = "Bye";
+        h1_text.style.color = "blue";
+    } else{
+        h1_text.textContent = "Hello";
+        h1_text.style.color = "white";
     }
 }
