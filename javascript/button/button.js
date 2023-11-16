750b2e99a24a0f62e557360c68f33d1652a2836d
keily6am <143549225+keily6am@users.noreply.github.com>
2023-11-16 22:47:04 +0000

    modified button modified button modified button

--- javascript/button/button.js
+++ javascript/button/button.js
@@ -1,6 +1,8 @@
 let h1_text = document.querySelector("#text");
 let p_tag = document.querySelector("p");
 let btn = document.querySelector(" #myButton"); 
+let btn2 = document.querySelector(" #greenbut");
+let btn3 = document.querySelector(" #bluebut");  
 
 //p_tag.innerHTML = h1_text; 
 btn.addEventListener("click",button_clicked);
@@ -10,7 +12,7 @@ function button_clicked()
     if(h1_text.textContent == "Hello")
     {
         h1_text.textContent = "Bye";
-        h1_text.style.color = "blue";
+        h1_text.style.color = "red";
     } else{
         h1_text.textContent = "Hello";
     }
