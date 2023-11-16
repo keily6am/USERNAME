df871e225b6c3729025372741dbacee225c27271
keily6am <143549225+keily6am@users.noreply.github.com>
2023-11-16 21:02:29 +0000

    renamed alert renamed index renamed styles modified button

--- javascript/button/button.js
+++ javascript/button/button.js
@@ -1,2 +1,6 @@
 let h1_text = document.querySelector("#text");
+let p_tag = document.querySelector("p");
+let btn = document.querySelector(#myButton); 
+
+//p_tag.innerHTML = h1_text; 
 console.log(h1_text);
