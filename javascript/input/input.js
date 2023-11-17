d5a133f9bc5f6bf0a8cc2a949cb516a6a4dc621e
keily6am <143549225+keily6am@users.noreply.github.com>
2023-11-17 20:48:47 +0000

    modified index modified index

--- javascript/input/input.js
+++ javascript/input/input.js
@@ -1,9 +1,13 @@
-let btn = document.querySelector("myButton");
+let btn = document.querySelector("#myButton");
 let input = document.querySelector("#input1");
+let pick = document.querySelector("#picked");
 
-button.addEventListener("click", button_clicked);
+btn.addEventListener("click", button_clicked);
 
 function button_clicked()
 {
     alert(input.value);
+     pick.textContent=input.value;
+     input.value = "";
+
 }
