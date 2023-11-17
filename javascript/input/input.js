80e309ed2178b0a5f3ad4870e1369b9da2444fe6
keily6am <143549225+keily6am@users.noreply.github.com>
2023-11-17 20:40:57 +0000

    modified input modified input

---
+++ javascript/input/input.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector("myButton");
+let input = document.querySelector("#input1");
+
+button.addEventListener("click", button_clicked);
+
+function button_clicked()
+{
+    alert(input.value);
+}
