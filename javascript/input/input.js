c0f3135eebd16f8fdd0c6f1efe4206f36f87e456
keily6am <143549225+keily6am@users.noreply.github.com>
2023-11-17 21:03:18 +0000

    modified input

--- javascript/input/input.js
+++ javascript/input/input.js
@@ -7,7 +7,14 @@ btn.addEventListener("click", button_clicked);
 function button_clicked()
 {
     alert(input.value);
-     pick.textContent=input.value;
+     pick.textContent=input.value + " " + pick.textContent;
      input.value = "";
 
+}
+
+let fname= "Keily";
+
+for(let i=0;i<fname.length; i++)
+{
+    alert(fname[i].toUpperCase());
 }
