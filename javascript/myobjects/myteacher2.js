63605e37210c1d7ababa7dd678ce829802194b02
keily6am <143549225+keily6am@users.noreply.github.com>
2023-12-12 21:06:05 +0000

    new file myteacher2

---
+++ javascript/myobjects/myteacher2.js
@@ -0,0 +1,10 @@
+const arr1 [1,2,3,4,5];
+
+console.log(arr1);
+
+// remove the last element/object of the array
+arr1.pop();
+
+// remove the first element of the array
+arr1.shift()
+console.log(arr1);
