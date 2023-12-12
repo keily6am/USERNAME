95f89fde1087e1d40e9477bbbcbdfffb56d3d67d
keily6am <143549225+keily6am@users.noreply.github.com>
2023-12-12 21:12:30 +0000

    modified myteacher2

--- javascript/myobjects/myteacher2.js
+++ javascript/myobjects/myteacher2.js
@@ -1,10 +1,18 @@
-const arr1 [1,2,3,4,5];
+const arr1 = [1,2,3,4,5];
 
 console.log(arr1);
 
 // remove the last element/object of the array
-arr1.pop();
+//arr1.pop();
 
 // remove the first element of the array
-arr1.shift()
+//arr1.shift()
+
+//remove something in between
+// .splice (location how many)
+//arr1.splice(2,2);
+
+// add something to front of the list
+arr1.unshift(6);
+
 console.log(arr1);
