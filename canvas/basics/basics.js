6bd2685bd1b7a2f1ba31bb617322caa912e3ab6d
keily6am <143549225+keily6am@users.noreply.github.com>
2024-02-02 20:44:05 +0000

    modified basics

---
+++ canvas/basics/basics.js
@@ -0,0 +1,8 @@
+// setup canvas for drawing 
+let WIDTH = 800;
+let HEIGHT = 600; 
+
+// get access to canvas element
+let canvas = document.getElementById("canvas");
+canvas.width = WIDTH; 
+canvas.height = HEIGHT;
