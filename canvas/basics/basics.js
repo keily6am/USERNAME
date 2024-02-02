e30c9a50fc2b7250180a881223d7c98b2647cdfb
keily6am <143549225+keily6am@users.noreply.github.com>
2024-02-02 20:50:32 +0000

    modified basics

--- canvas/basics/basics.js
+++ canvas/basics/basics.js
@@ -6,3 +6,9 @@ let HEIGHT = 600;
 let canvas = document.getElementById("canvas");
 canvas.width = WIDTH; 
 canvas.height = HEIGHT;
+// capital words means it won't change anymore; written once and will not change the whole game
+//canvas.style.background = "blue"; 
+
+// ctx is what we will call when we want to draw to canvas 
+let ctx = canvas.getContext("2d");
+
