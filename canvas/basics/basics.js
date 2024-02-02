954802c608a50ce4d64a807ebb8c1b5ff91ce945
keily6am <143549225+keily6am@users.noreply.github.com>
2024-02-02 21:20:22 +0000

    modified basics

--- canvas/basics/basics.js
+++ canvas/basics/basics.js
@@ -27,12 +27,35 @@ class Rectangle
     draw(ctx)
     {
         ctx.fillstyle = this.color; 
-        ctx.fillRect(this.xpos, this.ypos, this.width, this.height );
+        ctx.fillRect(this.xpos,this.ypos,this.width,this.height);
+    }
+}
+
+class Circle
+{
+    constructor(xpos,ypos,raidus, color)
+    {
+        this.xpos = xpos;
+        this.ypos = ypos;
+        this.raidus = raidus;
+        this.color = color;
+    }
+
+    draw(ctx)
+    {
+        ctx.fillstyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.xpos,this.ypos,this.raidus,0,2*Math.PI,false);
+        // in raidans when drawing in javascript, false/true = counterclockwise or clockwise. 
+        ctx.fill();
+        ctx.closePath();
     }
 }
 
 //create a rectangle with the given info.
-rect1= new Rectangle(0,0, 100, 100, "red");
+rect1 = new Rectangle(0,0, 100, 100, "red");
 rect1.draw(ctx); // pass in the context 
 rect2 = new Rectangle( 200, 200, 50, 10, "white");
 rect2.draw(ctx); 
+
+let circle1 = new Circle(WIDTH/2,HEIGHT/2,20,"green");
