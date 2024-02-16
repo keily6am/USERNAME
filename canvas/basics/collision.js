2085e09f27b7d9d8abee3a5c9f9648ced69aef8b
keily6am <143549225+keily6am@users.noreply.github.com>
2024-02-16 20:39:04 +0000

    modified collision

--- canvas/basics/collision.js
+++ canvas/basics/collision.js
@@ -8,10 +8,8 @@ cvs.height = HEIGHT;
 
 let ctx = canvas.getContext("2d");
 
-class Circle
-{
-    constructor(x,y,radius,color,dx,dy)
-    {
+class Circle {
+    constructor(x, y, radius, color, dx, dy) {
         this.x = x;
         this.y = y;
         this.radius = radius;
@@ -19,103 +17,89 @@ class Circle
         this.dy = dy;
         this.color = color;
         this.width = 10;
-        this.hit_counter= 0;
+        this.hit_counter = 0;
 
     }
 
-    draw(ctx)
-    {
+    draw(ctx) {
         ctx.stokeStyle = this.color;
         ctx.lineWidth = this.width;
         ctx.font = "12px Arial";
         ctx.textAlign = "center";
         ctx.textBaseline = "middle";
 
-        ctx.fillText(this.hit_counter,this.x,this.y);
+        ctx.fillText(this.hit_counter, this.x, this.y);
         ctx.beginPath();
-        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         ctx.stoke();
         ctx.closePath();
 
     }
 
 
-update()
-{
-    this.x = this.x + this.dx;
-    this.y = this.y + this.dy;
+    update() {
+        this.x = this.x + this.dx;
+        this.y = this.y + this.dy;
 
-    //hit the left wall
-    if(this.x + this.radius >= WIDTH)
-    {
-        this.dx = this.dx * -1;
-        this.hit_counter++;
+        //hit the left wall
+        if (this.x + this.radius >= WIDTH) {
+            this.dx = this.dx * -1;
+            this.hit_counter++;
+        }
+        //hit the right wall
+        if (this.x - this.radius <= 0) {
+            this.dx = this.dx * -1;
+            this.hit_counter++;
+        }
+        //hit the bottom wall
+        if (this.y + this.radius >= HEIGHT) {
+            this.dy = this.dy * -1;
+            this.hit_counter++;
+        }
+        //hit the top wall
+        if (this.y + this.radius <= 0) {
+            this.dy = this.dy * -1;
+            this.hit_counter++;
+        }
     }
-    //hit the right wall
-    if(this.x - this.radius <= 0)
-    {
-        this.dx = this.dx * -1;
-        this.hit_counter++;
+    get_x() {
+        return this.x;
     }
-    //hit the bottom wall
-    if(this.y + this.radius >= HEIGHT)
-    {
-        this.dy = this.dy * -1;
-        this.hit_counter++;
+
+    get_y() {
+        return this.y;
     }
-    //hit the top wall
-    if(this.y + this.radius <=0)
-    {
-        this.dy = this.dy * -1;
-        this.hit_counter++;
+
+    get_radius() {
+        return this.radius;
+    }
+    set_color(color) {
+        this.color = color;
     }
 }
-get_x()
-{
-    return this.x;
-}
 
-get_y()
-{
-    return this.y;
-}
+let circle1 = new Circle(20, 20, 20, "black", 4, 2);
+let circle2 = new Circle(WIDTH / 2, HEIGHT / 2, 300, "black", 0, 0);
 
-get_radius()
-{
-    return this.radius;
-}
-set_color(color)
-{
-    this.color = color;
-}
-}
-
-let circle1 = new Circle(20,20,20,"black",4,2);
-let circle2 = new Circle(WIDTH/2, HEIGHT/2, 300,"black",0,0);
-
-function get_distance(x1,y1,x2,y2)
-{
-    return Math.sqrt(Math.pow(x2-x1,2)+ Math.pow(y2,y1,2));
+function get_distance(x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2, y1, 2));
 }
 //game loop
-function animate(){
+function animate() {
     //clear context 
-    ctx.clearRect(0,0,WIDTH,HEIGHT);
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
 
     //update positions
     circle1.update();
     circle2.update();
 
     //check for collisions
-    if(get_distance(circle1.get_x(),circle1.get_y(),circle2.get_x(),circle2.get_y()
-                    <= circle1.get_radius()+circle2.get_radius()))
-    
-    {
+    if (get_distance(circle1.get_x(), circle1.get_y(), circle2.get_x(), circle2.get_y()
+        <= circle1.get_radius() + circle2.get_radius())) {
         circle1.set_color("red");
         circle2.set_color("red");
     }
-    else
-    {
+    else {
         circle1.set_color("black");
         circle2.set_color("black");
     }
