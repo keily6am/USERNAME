43e5a0b46ee9302670034d1f3af5115100382b63
keily6am <143549225+keily6am@users.noreply.github.com>
2024-04-29 17:25:58 +0000

    modified snake

--- canvas/ap project/snake.js
+++ canvas/ap project/snake.js
@@ -82,4 +82,90 @@ get_rad()
 set_color(color)
 {
     this.color = color;
+
+}
+class Square{
+    constructor(x,y,length,color,dx,dy) 
+    {
+        this.xpos=x;
+        this.ypos=y;
+        this.length=length;
+        this.color=color;
+        this.dx=dx;
+        this.dy=dy;
+        this.width=3;
+        this.hit_counter=0;
+
+    }
+}
+
+draw(ctx)
+{
+    ctx.fillStyle = "white";
+    ctx.lineWidth = this.width;
+    ctx.font = "12px Arial";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+
+    ctx.fillText(this.hit_counter, this.xpos, this.ypos);
+    ctx.beginPath();
+    ctx.rect(this.xpos - this.length / 2, this.ypos - this.length / 2, this.length, this.length);
+    ctx.fill();
+    ctx.closePath();
+
+}
+
+update(){
+    this.xpos += this.dx;
+    this.ypos += this.dy;
+
+    //checking for collisions with the walls
+    if (this.xpos + this.length >= WIDTH || this.xpos - this.length <=0){
+        this.dx= -this.dx;
+    }
+    if(this.ypos + this.length >= HEIGHT || this.ypos - this.length <= 0){
+        this.dy= -this.dy;
+    }
+
+
+get_x(){
+    return this.xpos;
+}
+
+set_dx(dx)
+{
+    this.dx =dx;
+}
+
+get_y()
+{
+    return this.ypos;
+}
+
+set_dy(dy)
+{
+    this.dy=dy;
+}
+
+set_color(color)
+{
+    this.color=color;
+
+}}
+
+//distance formula
+
+function get_distance(x1,y1,x2,y2)
+{
+    return Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
+}
+
+let square1 = new Square(WIDTH/2,HEIGHT/2,40, "black", 0,0); //cenetered inital position 
+let score = 0;
+
+let circles= [];
+let n = 10;
+for (let i =0;i<n; i++)
+{
+    let x = Math.random() * WIDTH;
 }
